bbbbc6fa1510cbe6f634af22a8cb18b023da5022
JnsJensen <jens.jens@live.dk>
2021-05-13 13:35:10 +0200

    Login Screen final draft

--- src/components/LoginBox.vue
+++ src/components/LoginBox.vue
@@ -1,11 +1,9 @@
 <template>
   <div id="login-box">
-    <!-- <img src="../assets/logo_small.png" alt="CEPTO"> -->
-    <h5>Username</h5>
-    <input type="text" name="username" v-model="input.username" placeholder="Username" />
-    <h5>Password</h5>
-    <input type="password" name="password" v-model="input.password" placeholder="Password" />
-    <button type="button" @click="on_login">Sign In</button>
+  <img src="../assets/logo_85_white.png" alt="CEPTO">
+  <input type="text" name="username" v-model="input.username" placeholder="Username" />
+  <input type="password" name="password" v-model="input.password" placeholder="Password" />
+  <button type="button" @click="on_login">Sign In</button>
   </div>
 </template>
 
@@ -13,43 +11,63 @@
 export default {
   name: 'LoginBox',
   data() {
-    return {input: {username: "", password: ""}};
+  return {input: {username: "", password: ""}};
   },
   methods: {
-    on_login() {
-      return;
-    }
+  on_login() {
+    return;
+  }
   }
 }
 </script>
 
 <style scoped>
 #login-box {
-  height: 300px;
+  height: 400px;
   width: 300px;
   margin: 25vh 0 0 0;
   padding: 50px;
   background-color: #2C2C2C;
   border-radius: 10px;
   box-shadow: 0 3px 6px #191919;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 }
 #login-box > input {
-    border: none;
-    width: 280px;
-    padding: 10px 10px;
-    font-size: 16px;
-    margin: 5px 0 20px 0;
-    border-radius: 10px;
-    background-color: #1A1A1A;
+  border: none;
+  width: 280px;
+  padding: 10px 10px;
+  font-size: 16px;
+  border-bottom: 2px solid #43b2e4;
+  background-color: #2C2C2C;
+  caret-color: white;
+  color: white;
+}
+#login-box > input:-webkit-autofill {
+  -webkit-box-shadow: 0 0 0px 1000px #2C2C2C inset;
+  -webkit-text-fill-color: white;
 }
 #login-box > button {
-    border: none;
-    padding: 10px;
-    width: 100%;
-    color: #A9A9A9;
-    font-size: 16px;
-    border-radius: 10px;
-    background-color: #1A1A1A;
-    margin: 20px 0 0 0;
+  border: none;
+  padding: 10px;
+  width: 100%;
+  color: white;
+  font-size: 16px;
+  border-radius: 10px;
+  background: linear-gradient(200deg, rgba(67, 178, 228, 0), rgba(67, 178, 228, 0.8) 100%),
+              linear-gradient(20deg, rgba(34, 43, 98, 0), rgba(34, 43, 98, 0.7) 70%),
+              linear-gradient(120deg, rgba(67, 178, 228, 0), rgba(67, 178, 228, 1)),
+              linear-gradient(320deg, rgba(34, 43, 98, 0), rgba(34, 43, 98, 1));
+  box-shadow: 0 3px 6px #191919;
+  transition: ease-in-out 100ms;
+}
+#login-box > button:hover {
+  transform: translateY(-3px);
+  box-shadow: 0 6px 12px #191919;
+}
+#login-box > button:active {
+  transform: translateY(4px);
+  box-shadow: 0 1px 2px #191919;
 }
 </style>
