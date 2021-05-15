9986974ce8baf2e1eeb95559149acb54d93b485e
JnsJensen <jens.jens@live.dk>
2021-05-15 10:36:42 +0200

    Clean up

--- src/main.js
+++ src/main.js
@@ -1,29 +1,8 @@
-// //import { createApp } from 'vue'
-// import Vue from 'vue'
-// //import App from './App.vue'
-// import VueRouter from 'vue-router'
-
-// import LoginScreen from "./components/LoginScreen.vue"
-// import LogTable from "./components/LogTable.vue"
-
-// Vue.use(VueRouter)
-
-
-// new Vue({
-//     router
-// }).$mount('#app')   
-
-// //export default app;
-// //createApp(App).mount('#app');
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '../router'
 
 const app = createApp(App);
-
-// app.config.globalProperties.$input.username = "";
-// app.config.globalProperties.$input.password = "";
 
 app.config.globalProperties.$user = {
     username: "",
@@ -42,9 +21,8 @@ app.config.globalProperties.$accounts = [
 ]
 
 app.config.globalProperties.$http = {
-    ip: "localhost",
+    ip: "10.9.0.221",
     command: "fecth-events"
 }
 
 app.use(router).mount('#app')
-// createApp(App).use(router).mount('#app')
