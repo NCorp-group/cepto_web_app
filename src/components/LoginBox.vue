e961e5f5efbafb38efb0c2cbb5d313940ab54fe4
JnsJensen <jens.jens@live.dk>
2021-05-22 09:45:10 +0200

    LoginBox comments

--- src/components/LoginBox.vue
+++ src/components/LoginBox.vue
@@ -15,10 +15,15 @@ export default {
   name: 'LoginBox',
   data() {
     return {
+      // Input object
+      // Stores the provided credentials temporarily for authorisation
       input: {
         username: "",
         password: ""
       },
+      // Feedback object
+      // Bound to the paragraph <p></p> element with v-bind
+      // Determines text and its colour
       feedback: {
         color: "transparent",
         text: "No input"
@@ -26,20 +31,31 @@ export default {
     };
   },
   methods: {
+    // Sign in callback method
+    // Is called when "Sign In" button is pressed or when enter is pressed in either input field "username" or "password"
+    // Gives the user feedback on the credentials they have put in
     on_login() {
+      // Case: The user has provided no credentials
       if ((this.input.username == "" || this.input.username == null) &&
            this.input.password == "" || this.input.password == null) {
+        
+        // Update feedback message
         this.feedback.color = "red";
         this.feedback.text = "No credentials were given";
         
         console.log(this.feedback.text);
       }
+      // Case: The credentials the user has provided matches a user in the system
+      // Done with a call to the check_credentials method
       else if(this.check_credentials()) {
+        // Store the provided credentials in the global variables
         this.$user.username = this.input.username;
         this.$user.password = this.input.password;
 
+        // Route the user to LogsScreen.vue
         this.$router.push({ path: "/logs" });
 
+        // Update feedback message
         this.feedback.color = "transparent";
         this.feedback.text = "Credentials authenticated";
 
@@ -47,23 +63,32 @@ export default {
 
         this.clear_credentials();
       }
+      // Case: The user provides wrong credentials
       else {
+        // Update feedback message
         this.feedback.color = "red";
         this.feedback.text = "Wrong credentials";
         
         console.log(this.feedback.text);
       }
     },
+    // Credential checker
+    // Checks whether the provided credentials match with registered users
     check_credentials() {
+      // Expects no match
       var is_match = false;
+      // Look for match in global accounts list
       this.$accounts.forEach(account => {
+        // Case: Match found, update bool to true
         if (this.input.username === account.username &&
             this.input.password === account.password) {
           is_match = true;
         }
       });
+      // Return access bool to caller
       return is_match;
     },
+    // Clears the input fields
     clear_credentials() {
       this.input.username = "";
       this.input.password = "";
